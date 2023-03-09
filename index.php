fc1b5ff90f4bc376cbbf35721cb88c0942ac6b0c
zulbaeti26 <127166433+zulbaeti26@users.noreply.github.com>
2023-03-09 08:03:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,439 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHEONSA HEALTHCARE</title>
+
+    <!-- aos css file cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+    <!-- magnific popup css cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+
+    <!-- bootstrap cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+
+<!-- header section starts  -->
+
+<header>
+
+<div class="container">
+
+    <a href="#" class="logo"><span>Cheonsa  </span><span>H</span>ealth<span>C</span>are.</a>
+
+    <nav class="nav">
+        <ul>
+            <li><a href="#home">home</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#facility">facility</a></li>
+            <li><a href="#review">review</a></li>
+            <li><a href="#contact">contact</a></li>
+            <li><a href="#post">post</a></li>
+        </ul>
+    </nav>
+
+    <div class="fas fa-bars"></div>
+
+</div>
+
+</header>
+
+<!-- header section ends  -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+<div class="container">
+
+    <div class="row min-vh-100 align-items-center text-center text-md-left">
+
+        <div class="col-md-6 pr-md-5" data-aos="zoom-in">
+            <img src="images/home-img.svg" width="100%" alt="">
+        </div>
+
+        <div class="col-md-6 pl-md-5 content" data-aos="fade-left">
+            <h1><span>stay</span> safe, <span>stay</span> healthy.</h1>
+            <h3>caring for you.</h3>
+            <a href="#"><button class="button">learn more</button></a>
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+
+<!-- home section ends -->
+
+<!-- about section start  -->
+
+<section class="about" id="about">
+
+<div class="container">
+
+    <div class="row min-vh-100 align-items-center">
+
+        <div class="col-md-6 content" data-aos="fade-right">
+
+            <div class="box">
+                <h3> <i class="fas fa-ambulance"></i> Ambulance Services </h3>
+                <p>Cheonsa HealthCare is equipped with various services ready to serve you 24 hours non-stop. Ambulances equipped with paramedics and various facilities to help patients on their way.</p>
+            </div>
+
+            <div class="box">
+                <h3> <i class="fas fa-procedures"></i> Emergency Rooms </h3>
+                <p>Emergency rooms which is ready to serve patients with the availability of emergency room doctors and paramedics at any time.</p>
+            </div>
+
+            <div class="box">
+                <h3> <i class="fas fa-stethoscope"></i> Free Check-up</h3>
+                <p>This service is handled by professional specialist doctors who are experienced in their fields. In addition, this service is also equipped with services and facilities such as non-invasive cardiovascular diagnostic examinations, radiology of the heart and blood vessels as well as various other things.</p>
+            </div>
+
+        </div>
+
+        <div class="col-md-6 d-none d-md-block" data-aos="fade-left">
+            <img src="images/about-img.png" width="100%" alt="">
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- facility section starts  -->
+
+<section class="facility" id="facility">
+
+<div class="container">
+
+<h1 class="heading"><span>'</span> our facilities <span>'</span></h1>
+
+<div class="box-container">
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img1.jpg" title="our team">
+            <img src="images/img1.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img2.jpg" title="our lab">
+            <img src="images/img2.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img3.jpg" title="emergency rooms">
+            <img src="images/img3.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img4.jpg" title="expert doctors">
+            <img src="images/img4.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img5.jpg" title="expert doctors">
+            <img src="images/img5.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img6.jpg" title="emergency rooms">
+            <img src="images/img6.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img7.jpg" title="expert doctors">
+            <img src="images/img7.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img8.jpg" title="emergency rooms">
+            <img src="images/img8.jpg" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="images/img9.jpg" alt="">
+        </a>
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- facility section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+<div class="container">
+
+<h1 class="heading"><span>'</span> people's review <span>'</span></h1>
+
+<div class="box-container">
+
+    <div class="box" data-aos="fade-right">
+        <p>Thank you Cheonsa HealthCare for helping me through the bad moments when I got sick. The staffs, nurse, and doctor are helpful! Happy to be there and will back if need the medication.</p>
+        <h3>Joshua Hong</h3>
+        <span>Des 5, 2022</span>
+        <img src="images/pic1.jpg" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-up">
+        <p>The staff are helpful and the doctors are friendly. The facilities are good. The enviroment is warm, nice dan clean. The wait isn't too long.</p>
+        <h3>Yoon Jeonghan</h3>
+        <span>Des 10, 2022</span>
+        <img src="images/pic2.jpg" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-left">
+        <p>Very modern and clean healthcare although it's a little bit small for a hospital. The atmosphere is quite good even in this covid era. The staffs are helpful and nice.</p>
+        <h3>Jeon Wonwoo</h3>
+        <span>Des 15, 2022</span>
+        <img src="images/pic3.jpg" alt="">
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- review section ends  -->
+
+<!-- counter section starts  -->
+
+<section class="counter">
+
+    <div class="container">
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-hospital"></i>
+                <span>120+</span>
+                <h3>Hospitals</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-users"></i>
+                <span>100+</span>
+                <h3>staff</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-smile"></i>
+                <span>1200+</span>
+                <h3>happy patients</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-procedures"></i>
+                <span>130+</span>
+                <h3>bed facility</h3>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- counter section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <div class="container min-vh-100">
+
+        <div class="row justify-content-center">
+
+            <h1 class="heading"><span>'</span> make an appointment <span>'</span></h1>
+
+            <div class="col-md-10" data-aos="flip-down">
+
+                <form action="">
+
+                    <div class="inputBox">
+                        <input type="text" placeholder="full name">
+                        <input type="number" placeholder="phone">
+                    </div>
+
+                    <div class="inputBox">
+                        <input type="email" placeholder="your email">
+                        <select name="" id="">
+                            <option value="" disabled selected>make an appointment</option>
+                            <option value="09-11 am">09-11 am</option>
+                            <option value="11-03 pm">11-03 pm</option>
+                            <option value="03-06 pm">03-06 pm</option>
+                            <option value="06-09 pm">06-09 pm</option>
+                        </select>
+                    </div>
+
+                    <textarea name="" id="" cols="30" rows="10" placeholder="message ( optional )"></textarea>
+
+                    <input type="submit" name="" id="" value="make appointment" class="button">
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- post section starts  -->
+
+<section class="post" id="post">
+
+    <div class="container min-vh-100">
+
+        <h1 class="heading"><span>'</span> our posts <span>'</span></h1>
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-right">
+                <img src="images/img2.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2023</span>
+                    <a href="#"><h3>Ambulance Services</h3></a>
+                    <p>Cheonsa HealthCare is equipped with various services ready to serve you 24 hours non-stop. Ambulances equipped with paramedics and various facilities to help patients on their way.</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <img src="images/img4.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2023</span>
+                    <a href="#"><h3>Emergency Rooms</h3></a>
+                    <p>Emergency Rooms, which is ready to serve patients with the availability of emergency room doctors and paramedics at any time.</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+                <img src="images/img5.jpg" alt="">
+                <div class="content">
+                    <span>jan 5, 2023</span>
+                    <a href="#"><h3>Free Check-up</h3></a>
+                    <p>This service is handled by professional specialist doctors who are experienced in their fields. In addition, this service is also equipped with services and facilities such as non-invasive cardiovascular diagnostic examinations, radiology of the heart and blood vessels as well as various other things.</p>
+                    <a href="#"><button class="button">learn more</button></a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- post section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4" data-aos="fade-right">
+                <a href="#" class="logo"><span>Cheonsa </span><span>H</span>ealth<span>C</span>are.</a>
+                <p>Cheonsa HealthCare is a leading health service provider in Indonesia that has quality health service standards in Indonesia.</p>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-up">
+                <h3>links</h3>
+                <a href="#">home</a>
+                <a href="#">about</a>
+                <a href="#">facility</a>
+                <a href="#">review</a>
+                <a href="#">contact</a>
+                <a href="#">post</a>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <h1 class="credit text-center mx-auto">Created For <span>Responsi 2023</span> | all rights reserved.</h1>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jquery cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- magnific popup js link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+<!-- aos js file cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<!-- custom js link  -->
+<script src="js/main.js"></script>
+
+
+<script>
+
+AOS.init({
+    duration:1000,
+    delay:400
+});
+
+</script>
+
+</body>
+</html>
